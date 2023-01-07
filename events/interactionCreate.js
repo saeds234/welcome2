3c9c2c108f91bf33c9615c60219cb49c7ca89982
saeds234 <121823596+saeds234@users.noreply.github.com>
2023-01-07 14:34:11 +0300

    Add files via upload

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,21 +2,11 @@ const { Collection, EmbedBuilder } = require("discord.js");
 const db = require("croxydb");
 const { readdirSync } = require("fs");
 
-const Rank = require("../func/Rank.js");
-const AddRank = require("../func/AddRank.js")
-const RemoveRank = require("../func/RemoveRank.js")
-
 module.exports = async(client, interaction) => {
 
-  if(interaction.isButton()) {
-    if(interaction.customId === `clear_${interaction.user.id}`) {
-        interaction.message.delete();
-    } 
-  }   
-
-  
-  
   if(interaction.isChatInputCommand()) {
+
+    if (!interaction.guildId) return;
 
     readdirSync('./commands').forEach(f => {
 
@@ -24,13 +14,16 @@ module.exports = async(client, interaction) => {
 
       if(interaction.commandName.toLowerCase() === cmd.name.toLowerCase()) {
 
-        return cmd.run(client, interaction, db, Rank, AddRank, RemoveRank);
+        return cmd.run(client, interaction, db);
 
 
       }
 
 
     });
+
+
+
   }
 
 };
--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,22 @@
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v10");
 const { TOKEN } = require("../config.json");
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const INTENTS = Object.values(GatewayIntentBits);
+const PARTIALS = Object.values(Partials);
+
+const client = new Client({
+    intents: INTENTS,
+    allowedMentions: {
+        parse: ["users"]
+    },
+    partials: PARTIALS,
+    retryLimit: 3
+});
 
 module.exports = async (client) => {
 
-  const rest = new REST({ version: "10" }).setToken(TOKEN);
+  const rest = new REST({ version: "10" }).setToken(TOKEN || process.env.token);
   try {
     await rest.put(Routes.applicationCommands(client.user.id), {
       body: client.commands,
@@ -14,4 +26,5 @@ module.exports = async (client) => {
   }
 
     console.log(`${client.user.tag} Aktif!`);
+    client.user.setActivity(`Lourity Code | Botlist Botu`)
 };
